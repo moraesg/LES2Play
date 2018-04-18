b67e67c2094138be40fe851d7f4bedf6240cb5ee
moraesg <gabriel.moraes10@fatec.sp.gov.br>
2018-04-18 20:48:41 -0300

    api url's added. some shit repaired up.

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
-    res.send('index page');
+    res.send('load all users.');
+});
+
+router.get('/:id', function(req, res, next){
+    res.send('load a single user page.');
 });
 
 module.exports = router;
